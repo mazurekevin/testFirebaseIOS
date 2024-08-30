ea7dd89811433ff479c656c6b024c370f827f21a
mazurekevin <kevin.mazure.km@gmail.com>
2024-08-30 22:57:26 +0200

    Initial Commit

---
+++ testFirebaseIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testFirebaseIOS
+//
+//  Created by teamdoc on 30/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testFirebaseIOS/testFirebaseIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  testFirebaseIOSApp.swift
+//  testFirebaseIOS
+//
+//  Created by teamdoc on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct testFirebaseIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
