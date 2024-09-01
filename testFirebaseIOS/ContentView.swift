4aad05644b9af744061664d32bf2b11506b5fc9a
mazurekevin <kevin.mazure.km@gmail.com>
2024-09-01 23:18:21 +0200

    Test read Datas

--- testFirebaseIOS/ContentView.swift
+++ testFirebaseIOS/ContentView.swift
@@ -6,13 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseDatabase
 
 struct ContentView: View {
     @State var identitifiant: String = ""
     @State var password: String = ""
     
+    var ref = Database.database().reference()
+    
     func login(){
-        
+        ref.child("Users/2").observeSingleEvent(of: .value, with: { snapshot in
+            let result = snapshot.value //as? String? ?? "erreur"
+            dump(result)
+        }) { error in
+            print("Erreur lors de la récupération des données : \(error.localizedDescription)")
+        }
     }
     
     
